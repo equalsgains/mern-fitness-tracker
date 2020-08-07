c7fa95bfc3ce19bab12459f33ff7c2e0eb007be6
Luis Quintanilla <equalsgains@gmail.com>
2020-08-07 15:03:01 -0600

    tested backend with success

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,11 @@ connection.once("open", () => {
     console.log(("MongoDB database connection established successfully"));
 })
 
+const exercisesRouter = require("./routes/excercises");
+const usersRouter = require("./routes/users");
+
+app.use("/excercises", exercisesRouter);
+app.use("/users", usersRouter);
 
 app.listen(port, () => {
     console.log("Server is running on port: ${port}");
